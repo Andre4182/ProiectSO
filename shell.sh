4b2e7501d9e5f3c799bc734d6e2ebf38faed2d5b
Clipa Andreia <clipa.andreia@gmail.com>
2023-12-08 18:32:09 +0200

    modified shell for more clear usage message and handled non-alphanumeric argument

--- shell.sh
+++ shell.sh
@@ -1,15 +1,21 @@
 contor=0
 if test $# -eq 1
 then
-	while IFS= read -r line
-	do
-		if grep -Eq '^[A-Z][A-Za-z0-9 ,!.?]*[.!?]$' <<< "$line" && ! grep -Eq ',si' <<< "$line" && grep -Eq "$1" <<< "$line"
-		then
-		contor=$(expr "$contor" + 1)
+	if grep -Eq '[A-Za-z0-9]' <<< "$1"
+	then 
+		while IFS= read -r line
+		do
+			if grep -Eq '^[A-Z][A-Za-z0-9 ,!.?]*[.!?]$' <<< "$line" && ! grep -Eq ',si' <<< "$line" && grep -Eq "$1" <<< "$line"
+			then
+				contor=$(expr "$contor" + 1)
 
-		fi
-	done
-	echo $contor
+			fi
+		done
+		echo $contor
+	else 
+		echo "The given argument should be an alphanumeric character!";
+	fi
+	
 else
-	echo "Usage "
+	echo "Usage ./shell.sh <character>"
 fi
